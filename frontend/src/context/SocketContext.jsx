823088d02aad6ec7afc54865068b63c83bb5523c
varshith kalwa <varshith.kalwa25@gmail.com>
2024-05-04 20:16:59 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://moshi-moshi-d7ly.onrender.com", {
+			const socket = io("https://moshi-moshi-1.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
