6ce2c0881eac497a24589270234a171795a3d7f2
varshith kalwa <varshith.kalwa25@gmail.com>
2024-05-04 22:51:53 +0530

    update in socket

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-app.listen(port,()=> {
+server.listen(port,()=> {
     connectToMongoDB();
     console.log(`port is running at ${port}`);
 })
